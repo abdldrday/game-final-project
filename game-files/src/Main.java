d3209ef0d7358b1a607b2f0649b97be412d4bf99
hasnuq <hashym11@bk.ru>
2025-03-31 21:58:50 +0500

    line

--- game-files/src/Main.java
+++ game-files/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
             System.out.println("fkdsofsjkdngjksn");
+            System.out.println("erwerwerwer");
         }
 
         System.out.println("Tima and Hashym");
